55b98450b793f8d26f504b61edc7ae5cf437b2f5
Manu-Madhu <manum66466@gmail.com>
2025-03-21 01:30:32 +0530

    update the profile image

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,7 +1,7 @@
 import * as Icons from "react-icons/tb";
 import Input from '../common/Input.jsx';
 import Profile from '../common/Profile.jsx';
-import ProfileImg from '../../images/users/user_3.webp';
+import ProfileImg from '../../../public/dummy.jpg';
 import { useSelector } from "react-redux";
 
 const Navbar = () => {
