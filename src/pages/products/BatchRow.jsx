db2d12b247a3376c4a345fea51b45f624a6e809e
abhijth.r.b <abhijithrb91@gmail.com>
2025-04-01 21:02:10 +0530

    batch addtion working

---
+++ src/pages/products/BatchRow.jsx
@@ -0,0 +1,73 @@
+import Input from "../../components/common/Input.jsx";
+import React from 'react'
+
+const BatchRow = ({ batch, index, removeBatchRow, setProduct }) => {
+
+    const inputChangeHandler = (name, value) => {
+        setProduct((prev) => (
+            {
+                ...prev,
+                batches: prev?.batches?.map((item, idx) => idx === index ? { ...item, [name]: value } : item)
+            }
+        )
+        )
+    }
+
+
+    return (
+        <div className="flex flex-col gap-2">
+            <div className="flex gap-4">
+                <h5>Batch {index + 1} :</h5>
+
+                {
+                    index > 0
+                    &&
+                    <button
+                        onClick={() => removeBatchRow(index)}
+                        className="bg-red-500 text-white text-sm px-2 rounded-md" >Remove</button>
+                }
+            </div>
+            <div className="flex gap-2">
+                <Input
+                    type="text"
+                    placeholder="Enter the Batch Number"
+                    label="Batch Number"
+                    name="batchNumber"
+                    value={batch?.batchNumber}
+                    onChange={(value) => inputChangeHandler("batchNumber", value)}
+                />
+
+                <Input
+                    type="text"
+                    placeholder="Enter the Quantity"
+                    label="Quantity"
+                    name="quantity"
+                    value={batch?.quantity}
+                    onChange={(value) => inputChangeHandler("quantity", value)}
+                />
+
+                <Input
+                    type="date"
+                    placeholder="Enter Manufacturing Date"
+                    label="Manufacturing Date"
+                    name="mfgDate"
+                    value={batch?.mfgDate}
+                    onChange={(value) => inputChangeHandler("mfgDate", value)}
+                />
+
+                <Input
+                    type="date"
+                    placeholder="Enter Expiry Date"
+                    label="Expiry Date"
+                    name="expDate"
+                    value={batch?.expDate}
+                    onChange={(value) => inputChangeHandler("expDate", value)}
+                />
+
+            </div>
+        </div>
+
+    )
+}
+
+export default BatchRow
